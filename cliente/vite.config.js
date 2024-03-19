bff2dbd28240f8b180a2f027895780ae2920ebf9
JEFE DE DESARROLLO <jdesarrollo@JEFEDESARROLLO.commerk.local>
2024-03-18 20:57:31 -0500

    prueba 4

--- cliente/vite.config.js
+++ cliente/vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://encuesta-eight.vercel.app/admin-one-vue-tailwind/",
+  base: "/admin-one-vue-tailwind/",
   plugins: [vue()],
   resolve: {
     alias: {
